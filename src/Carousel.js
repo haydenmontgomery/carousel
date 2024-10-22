3f21404901a2496a965d86072cc8dcb45c63e1db
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-10-22 11:08:22 -0400

    Finished tests and fixed bugs of exhausing carousel images. Arrows dissappear when at the end left or right respectively.

--- src/Carousel.js
+++ src/Carousel.js
@@ -21,28 +21,41 @@ import Card from "./Card";
   const total = photos.length;
 
   //Increments currCardIdx state by 1
-  function goForward() {
-    setCurrCardIdx(currCardIdx + 1);
-  }
+  const goForward = () => {
+    if (currCardIdx < total -1) {
+      setCurrCardIdx(currCardIdx +1);
+    }
+  };
+
+  //Decrements currCardIdx state by 1
+  const goBackward = () => {
+    if (currCardIdx > 0) {
+      setCurrCardIdx(currCardIdx - 1);
+    }  
+  };
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
+        {currCardIdx > 0 && (
+          <i
           className="bi bi-arrow-left-circle"
-          onClick={goForward}
-        />
+          onClick={goBackward}
+          />
+        )}
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
+        {currCardIdx < total -1 && (
+          <i
           className="bi bi-arrow-right-circle"
           onClick={goForward}
-        />
+          />
+        )}
       </div>
     </div>
   );
--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,26 +1,26 @@
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
-import photos from "./photos.js";
 import TEST_IMAGES from "./_testCommon.js";
 
 // smoke test
 test('it renders without crashing', () => {
-  render(<Carousel photos={photos} title="Test"/>)
+  render(<Carousel photos={TEST_IMAGES} title="Test"/>)
 });
 
 
 test("it matches snapshot", () => {
-  const { asFragment } = render(<Carousel photos={photos} title="Test"/>)
+  const { asFragment } = render(<Carousel photos={TEST_IMAGES} title="Test"/>)
   expect(asFragment()).toMatchSnapshot();
 })
 
-it("works when you click on the right arrow", function() {
+it("works when you click on the right arrow then left arrow", function() {
   const { container } = render(
     <Carousel
       photos={TEST_IMAGES}
       title="images for testing"
     />
   );
+  console.log(container);
   // expect the first image to show, but not the second
   expect(
     container.querySelector('img[alt="testing image 1"]')
@@ -40,4 +40,53 @@ it("works when you click on the right arrow", function() {
   expect(
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
+
+  // move left in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
 });
+
+it("has the left arrow gone on first image, and right arrow gone on last image", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  //We are at the beginning of the images so left is gone.
+  expect(
+    container.querySelector(".bi-arrow-left-circle")
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+
+  //We are at the middle of the images so both should be here.
+  expect(
+    container.querySelector(".bi-arrow-left-circle")
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector(".bi-arrow-right-circle")
+  ).toBeInTheDocument();
+
+  //move forward in the carousel
+  fireEvent.click(rightArrow);
+
+  //We are at the end of the images so the right should be gone.
+  expect(
+    container.querySelector(".bi-arrow-left-circle")
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector(".bi-arrow-right-circle")
+  ).not.toBeInTheDocument();
+});  
